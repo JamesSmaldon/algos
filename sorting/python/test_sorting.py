469206b15abd9eb06c475d2db4f64411bf6a528f
james <James.Smaldon@gmail.com>
2015-11-03 23:22:32 +0000

    More Lomuto partition function tests.

--- sorting/python/test_sorting.py
+++ sorting/python/test_sorting.py
@@ -33,3 +33,22 @@ class LomutoPartition(unittest.TestCase):
         new_pivot = lomuto_partition(arr, 0, len(arr)-1)
         self.assertEqual(new_pivot, 4)
         self.assertEqual(arr, [1, 4, 2, 3, 6, 7])
+
+    def test_all_smaller_than_pivot(self):
+        self.assertEqual(1, lomuto_partition([1, 2], 0, 1))
+        self.assertEqual(2, lomuto_partition([1, 2, 3], 0, 2))
+        self.assertEqual(3, lomuto_partition([1, 2, 3, 4], 0, 3))
+
+    def test_all_larger_than_pivot(self):
+        arr = [5, 6, 7, 8, 4]
+        new_pivot = lomuto_partition(arr, 0, 4)
+
+        self.assertEqual(new_pivot, 0)
+        self.assertEqual(arr, [4, 6, 7, 8, 5])
+
+    def test_all_the_same_number(self):
+        arr = [0, 0, 0, 0]
+        new_pivot = lomuto_partition(arr, 0, 3)
+
+        self.assertEqual(new_pivot, 3)
+        self.assertEqual(arr, [0, 0, 0, 0])
