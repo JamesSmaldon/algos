d50d3825a4bfaf0b4c4255ba2768032fdc124162
james <James.Smaldon@gmail.com>
2015-11-03 23:01:14 +0000

    Started again with test driven quicksort.

--- sorting/python/sorting.py
+++ sorting/python/sorting.py
@@ -1,36 +1,22 @@
 __author__ = 'smaldon'
 
 
-def middle_element_pivot_index(seq):
-    # empty sequence case
-    if not seq:
-        return -1
-
-    half_seq_len = len(seq) / 2.0
-
-    return int(half_seq_len - 0.5)
-
-
-def partition_ranges(pivot, seq):
+def lomuto_partition(seq, start, end):
     seq_len = len(seq)
 
-    if seq_len < 3:
-        raise ValueError('partition_range sequence smaller than 2')
+    if start < 0 or start >= seq_len:
+        raise ValueError("Invalid Start Index")
 
-    if pivot < 0 or pivot >= seq_len:
-        raise ValueError('partition_range invalid pivot')
+    if end < 0 or end >= seq_len:
+        raise ValueError("Invalid End Index")
 
-    last_index = seq_len - 1
-    left_part = None if pivot == 0 else (0, pivot - 1)
-    right_part = None if pivot == last_index else (pivot + 1, last_index)
+    pivot = seq[end]
+    i = start
+    for j in range(i, end):
+        if seq[j] <= pivot:
+            seq[j], seq[i] = seq[i], seq[j]
+            i += 1
 
-    return left_part, right_part
+    seq[i], seq[end] = seq[end], seq[i]
 
-def exchange_elements(pivot, partitions, seq):
-    pass
-
-def quicksort(seq):
-    pass
-
-
-
+    return i
--- sorting/python/test_sorting.py
+++ sorting/python/test_sorting.py
@@ -1,71 +1,35 @@
 import unittest
 
-from sorting import middle_element_pivot_index, partition_ranges
+from sorting import lomuto_partition
 
 __author__ = 'smaldon'
 
 
-class MiddleElementPivotIndex(unittest.TestCase):
-    def testReturnsMinusOneForEmptySeq(self):
-        self.assertEqual(middle_element_pivot_index([]), -1)
-
-    def testReturnsZeroForOneElementSeq(self):
-        self.assertEqual(middle_element_pivot_index([0]), 0)
-
-    def testReturnsZeroForTwoElementSeq(self):
-        self.assertEqual(middle_element_pivot_index([0, 1]), 0)
-
-    def testReturnsMiddleIndexForOddSizedSeq(self):
-        self.assertEqual(middle_element_pivot_index([0, 1, 2, 3, 4]), 2)
-
-    def testReturnsLeftMiddleIndexForEvenSizedSeq(self):
-        self.assertEqual(middle_element_pivot_index([0, 1, 2, 3]), 1)
-
-
-class PartitionRanges(unittest.TestCase):
-    def testPivotIsFirstElement(self):
-        actual = partition_ranges(0, [1, 2, 3, 4])
-        expected = None, (1, 3)
-
-        self.assertEqual(actual, expected)
-
-    def testPivotIsLastElement(self):
-        actual = partition_ranges(3, [1, 2, 3, 4])
-        expected = (0, 2), None
-
-        self.assertEqual(actual, expected)
-
-    def testPivotIsMiddleInEvenSizedSeq(self):
-        actual = partition_ranges(1, [1, 2, 3, 4])
-        expected = (0, 0), (2, 3)
-
-        self.assertEqual(actual, expected)
-
-    def testPivotIsMiddleInOddSizedSeq(self):
-        actual = partition_ranges(2, [1, 2, 3, 4, 5])
-        expected = (0, 1), (3, 4)
-
-        self.assertEqual(actual, expected)
-
-    def testPivotIsSecondToLastElement(self):
-        actual = partition_ranges(6, [1, 2, 3, 4, 5, 6, 7, 8])
-        expected = (0, 5), (7, 7)
-
-        self.assertEqual(actual, expected)
-
-    def testThrowsForSingletonSeq(self):
+class LomutoPartition(unittest.TestCase):
+    def test_raises_value_error_for_start_index_out_of_bounds(self):
         with self.assertRaises(ValueError):
-            partition_ranges(0, [1])
+            lomuto_partition([1, 2, 3], -1, 0)
 
-    def testThrowsForTwoElementSeq(self):
         with self.assertRaises(ValueError):
-            partition_ranges(0, [1, 2])
+            lomuto_partition([1, 2, 3], 3, 0)
 
-    def testThrowsForNegativePivot(self):
+    def test_raises_value_error_for_end_index_out_of_bounds(self):
         with self.assertRaises(ValueError):
-            partition_ranges(-1, [1, 2])
+            lomuto_partition([1, 2, 3], 0, -1)
 
-    def testThrowsForPivotLargerThanSeq(self):
         with self.assertRaises(ValueError):
-            partition_ranges(3, [0, 1, 2])
+            lomuto_partition([1, 2, 3], 0, 3)
 
+    def test_doesnt_swap_elements_in_two_element_seq(self):
+        arr = [0, 1]
+        new_pivot = lomuto_partition(arr, 0, 1)
+
+        self.assertEqual(arr, [0, 1])
+        self.assertEqual(new_pivot, 1)
+
+    def test_partitions_numbers_in_seq(self):
+        arr = [1, 4, 7, 2, 3, 6]
+
+        new_pivot = lomuto_partition(arr, 0, len(arr)-1)
+        self.assertEqual(new_pivot, 4)
+        self.assertEqual(arr, [1, 4, 2, 3, 6, 7])
