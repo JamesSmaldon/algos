3a8ba8258e93ab77c1f764c6ca256ae78ed453ca
James Smaldon <James.Smaldon@gmail.com>
2016-03-15 21:02:13 +0000

    Move rect into the shapes module.

--- src/maths/shapes.js
+++ src/maths/shapes.js
@@ -21,6 +21,21 @@ M.Triangle = function(p1, p2, p3) {
     this.points = [p1, p2, p3];
 }
 
+M.Rect = function(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+}
+
+M.Rect.prototype.map = function(v, v_rect) {
+    var norm_x = (v.data[0] - v_rect.x) / v_rect.width;
+    var norm_y = (v.data[1] - v_rect.y) / v_rect.height;
+
+    return new M.Vector(this.x + norm_x * this.width,
+                        this.y + norm_y * this.height);
+}
+
 M.intersection = function(a, b) {
     var line_plane = function(line, plane) {
         var w = line.start.sub(plane.origin);
